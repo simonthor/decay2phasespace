b9f1c54f5bddbeb0d6a080c10ce4336d13a015a7
Simon Thor <simon11.thor@hotmail.se>
2021-08-05 17:09:34 +0200

    Add more test code to test_single_chain and check_norm

--- tests/test_fulldecay.py
+++ tests/test_fulldecay.py
@@ -18,8 +18,9 @@ def check_norm(full_decay: FullDecay, **kwargs) -> list[tuple]:
 
     Returns
     -------
-    tuple
-        All the values returned by generate, both times
+    list[tuple]
+        All the values returned by generate, both times.
+        The return arguments from normalize_weights=True is the first element in the returned list.
 
     Notes
     -----
@@ -29,15 +30,19 @@ def check_norm(full_decay: FullDecay, **kwargs) -> list[tuple]:
     for norm in (True, False):
         return_args = full_decay.generate(normalize_weights=norm, **kwargs)
         assert len(return_args) == 2 if norm else 3
+        assert sum(len(w) for w in return_args[0]) == kwargs['n_events']
+        if not norm:
+            assert all(len(w) == len(mw) for w, mw in zip(return_args[0], return_args[1]))
+
         all_return_args.append(return_args)
 
     return all_return_args
 
 
 def test_single_chain():
+    """Test converting a decaylanguage dict with only one possible decay."""
     container = FullDecay.from_dict(dplus_single)
     output_decay = container.gen_particles
-    print(output_decay)
     assert len(output_decay) == 1
     prob, gen = output_decay[0]
     assert_almost_equal(prob, 1)
@@ -45,17 +50,20 @@ def test_single_chain():
     assert {p.name for p in gen.children} == {'K-', 'pi+', 'pi+ [0]', 'pi0'}
     check_norm(container, n_events=1)
     check_norm(container, n_events=100)
+    (normed_weights, decay_list), _ = check_norm(container, n_events=100)
+    assert len(decay_list) == 1
+    events = decay_list[0]
+    assert set(events.keys()) == {'K-', 'pi+', 'pi+ [0]', 'pi0', 'gamma', 'gamma [0]'}
+    assert all(len(p) == 100 for p in events.values())
 
 
 def test_branching_children():
     container = FullDecay.from_dict(pi0_4branches)
     output_decays = container.gen_particles
-    print(output_decays)
     assert len(output_decays) == 4
     assert_almost_equal(sum(d[0] for d in output_decays), 1)
     check_norm(container, n_events=1)
-    check_norm(container, n_events=100)
-    # TODO add more asserts here
+    (normed_weights, events), _ = check_norm(container, n_events=100)
 
 
 def test_branching_grandchilden():
@@ -63,7 +71,7 @@ def test_branching_grandchilden():
     output_decays = container.gen_particles
     assert_almost_equal(sum(d[0] for d in output_decays), 1)
     check_norm(container, n_events=1)
-    check_norm(container, n_events=100)
+    (normed_weights, events), _ = check_norm(container, n_events=100)
     # TODO add more asserts here
 
 
@@ -72,5 +80,5 @@ def test_big_decay():
     output_decays = container.gen_particles
     assert_almost_equal(sum(d[0] for d in output_decays), 1)
     check_norm(container, n_events=1)
-    check_norm(container, n_events=100)
+    (normed_weights, events), _ = check_norm(container, n_events=100)
     # TODO add more asserts here
